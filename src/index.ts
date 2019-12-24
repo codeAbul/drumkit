d37781b73a7075ab04d3bd88c52fefd307569691
Abul Kalam Azad Shaik <saka16492@gmail.com>
2019-12-24 11:41:26 +0900

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+document.addEventListener("keyup", playTheAudio);
+
+function playTheAudio(event: KeyboardEvent, { target } = event) {
+  if (target instanceof HTMLElement) {
+    let keyPressed = event.key;
+    let relAudioEl = document.querySelector(`audio[data-key = ${keyPressed}`);
+    let relKeyEl = document.querySelector(`div[data-key=${keyPressed}]`);
+    relKeyEl.addEventListener("transitionend", stopTheAudio);
+    let currentClassList = Array.from(relKeyEl.classList);
+    relKeyEl.classList.add(`${currentClassList[0]}__clicked`);
+    if (relAudioEl instanceof HTMLAudioElement) {
+      relAudioEl.currentTime = 0;
+      relAudioEl.play();
+    }
+  }
+}
+function stopTheAudio({ target }: TransitionEvent) {
+  if (target instanceof HTMLElement) {
+    target.classList.remove(`keypanel--key__clicked`);
+  }
+}
